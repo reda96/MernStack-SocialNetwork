0dd62a752c119860f2cc34f2fcf02f961623320d
ahmedreda <ahmedreda7296@gmail.com>
2020-03-26 07:57:40 +0200

    Delete-experience-from-you-profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -201,5 +201,21 @@ router.put(
     }
   }
 );
-
+//  @route  Delete api/profile/experience/:exp_id
+//  @desc   Delete profile experience
+//  @access Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server error");
+  }
+});
 module.exports = router;
