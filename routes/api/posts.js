cf37ab1f7f9243af17f435f2b3e2ba473fb8e1fd
ahmedreda <ahmedreda7296@gmail.com>
2020-03-27 18:53:08 +0200

    CreateApost&getAllPosts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+//  @route  Post api/posts
+//  @desc   Create a post
+//  @access Private
+router.post(
+  "/",
+  [
+    auth,
+    check("text", "Text is required")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 //  @route  Get api/posts
-//  @desc   Test route
-//  @access Public
-router.get("/", (req, res) => res.send("Posts route"));
+//  @desc   Get all posts
+//  @access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await User.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server error");
+  }
+});
 module.exports = router;
