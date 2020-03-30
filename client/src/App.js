6093cf90c50266d1e2baaf146f11109ed7f71572
ahmedreda <ahmedreda7296@gmail.com>
2020-03-30 06:44:10 +0200

    add-CreateProfileComponent&CreateAndUpdateProfileAction

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import "./App.css";
 import { Provider } from "react-redux";
 import { loadUser } from "./actions/auth";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-form/CreateProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import setAuthToken from "./utils/setAuthToken";
 import store from "./store";
@@ -30,6 +31,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,13 +3,15 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE,
-  ACCOUNT_DELETED
+  ACCOUNT_DELETED,
+  CREATE_PROFILE,
+  UPDATE_PROFILE
 } from "./types";
 import axios from "axios";
 import { setAlert } from "./alert";
 export const getCurrentProfile = () => async dispatch => {
   try {
-    const res = await axios.get("/api/profiles/me");
+    const res = await axios.get("/api/profile/me");
 
     dispatch({
       type: GET_PROFILE,
@@ -39,5 +41,40 @@ export const deleteAccount = () => async dispatch => {
         payload: { msg: err.response.statusText, status: err.response.status }
       });
     }
+  }
+};
+
+// Create or update a profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const body = JSON.stringify(formData);
+    const res = await axios.post("/api/profile", body, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created"), "success");
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (error) {
+    const err = error.response.data.errors;
+    if (err) {
+      err.array.forEach(e => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
   }
 };
